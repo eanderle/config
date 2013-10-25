c1c771a37d7e8b4cba8fcfb97d5b2152ef54a9af
Eric Anderle <eric@twilio.com>
2013-10-24 23:54:15 -0700

    powerline oh-my-zsh

---
+++ oh-my-zsh/powerline.zsh
@@ -0,0 +1,3 @@
+if [[ -r ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh ]]; then
+    source ~/Library/Python/2.7/lib/python/site-packages/powerline/bindings/zsh/powerline.zsh
+fi
