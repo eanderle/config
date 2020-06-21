9b9d45c9091fd44955a865ea67cc49117cf5a22f
Eric Anderle <enanderle@gmail.com>
2020-06-20 23:14:45 -0400

    lunix: install sqlite3

--- lunix/new_machine/init.sh
+++ lunix/new_machine/init.sh
@@ -1,2 +1,7 @@
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y curl tmux synapse silversearcher-ag
+sudo apt-get install -y \
+    curl \
+    tmux \
+    synapse \
+    silversearcher-ag \
+    sqlite3 \
