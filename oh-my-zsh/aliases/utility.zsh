72a1f81ac75a48f9322b9f6fc5e4e8e9979f3aaa
Eric Anderle <eric@twilio.com>
2020-11-06 09:42:10 -0500

    alias: fix urlencode/decode, add epoch time

--- oh-my-zsh/aliases/utility.zsh
+++ oh-my-zsh/aliases/utility.zsh
@@ -1,8 +1,14 @@
 ## pretty printing
 alias pp='python -mjson.tool'
 alias px='xmllint --format -'
-alias urle='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+alias urld='python3 -c "import sys, urllib.parse as ul; \
+        print(ul.unquote_plus(sys.argv[1]))"'
+
+alias urle='python3 -c "import sys, urllib.parse as ul; \
+        print (ul.quote_plus(sys.argv[1]))"'
 
 alias ag="ag -i"
 
 alias ng="ngrok http -subdomain eric"
+
+alias epoch="date +'%s'"
