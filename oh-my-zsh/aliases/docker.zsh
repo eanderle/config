6556e6b9fb392916eea0d27f7863750a8737eb2b
Eric Anderle <eric@twilio.com>
2022-04-08 11:26:41 -0400

    docker, add gitconfig

--- oh-my-zsh/aliases/docker.zsh
+++ oh-my-zsh/aliases/docker.zsh
@@ -1,6 +1,10 @@
-alias dcub="docker-compose up --build"
-alias dcd="docker-compose down"
+alias dcub="docker compose up --build"
+alias dcd="docker compose down"
 
 function dbash() {
   docker container exec -it $(docker ps --filter name="$1" -q) /bin/bash
+}
+
+function dlogs() {
+  docker service logs --filter name="$1"
 }
